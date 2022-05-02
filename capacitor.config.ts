7339be37ed3c11a18e6e48ed5a4c73e4ffbef7c7
Jose Luis Mejia Rojas <joseluismejiarojas@gmail.com>
2022-05-01 20:06:08 -0500

    init project

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'angular-mobile-app',
+  // TODO: Change here with name project
+  webDir: 'dist/angular-mobile-app',
+  bundledWebRuntime: false
+};
+
+export default config;
